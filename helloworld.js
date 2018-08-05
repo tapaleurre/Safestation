b9cbd705e234b87d1613e548e55131a7d1c97026
Simon Foëx <tapaleurre@hotmail.fr>
2018-08-05 14:53:54 +0200

    Wew we let the token to be public

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const Token = 'MzczODMxNjE0OTc2ODg0NzM2.DkhoQQ.liVlEOovLqZtH1J1VS8frNbwp4w';
+const tokenModule = require('./token');
 
+Token = tokenModule.getToken();
 console.log(`Welcome to SafetyBot`);
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
